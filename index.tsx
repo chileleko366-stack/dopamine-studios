602e6405b7e50d4c181e3ddb5f0570a7120f5048
chileleko366-stack <chileleko366@gmail.com>
2026-05-05 09:28:47 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-// remotion/index.tsx
-// Entry point for all DopamineStudios mograph compositions
-// Runs inside GitHub Actions via: npx remotion render
-
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
-
-registerRoot(RemotionRoot);
